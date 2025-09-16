4a4f4ec395db1c70fea0fbbc9af5442f5ab330d1
Ryan Chen <wigwomm@gmail.com>
2025-09-16 16:53:52 -0400

    finished validation js

--- checkpoint1/signup_project/script.js
+++ checkpoint1/signup_project/script.js
@@ -1,15 +1,8 @@
 const form = document.getElementById('form')
-<<<<<<< HEAD
 const firstname_input = document.getElementById('firstname-input')
 const email_input = document.getElementById('email-input')
 const password_input = document.getElementById('password-input')
 const repeat_password_input = document.getElementById('repeat-password-input')
-=======
-const email_input = document.getElementById('email')
-const username_input = document.getElementById('username')
-const password_input = document.getElementById('password')
-const repeat_password_input = document.getElementById('repeatPassword')
->>>>>>> 7d90ef21c3aaf9ceeeb3981f3cec1049eb49d3e8
 const error_message = document.getElementById('error-message')
 
 form.addEventListener('submit', (e) => {
@@ -27,7 +20,6 @@ form.addEventListener('submit', (e) => {
   if(errors.length > 0){
     // If there are any errors
     e.preventDefault()
-<<<<<<< HEAD
     error_message.innerText  = errors.join(". ")
   }
 })
@@ -86,35 +78,3 @@ allInputs.forEach(input => {
     }
   })
 })
-=======
-    let errors = []
-    if(username_input){
-        errors = getSignupFormErrors(email_input.value, username_input.value, password_input.value,repeat_password_input)
-    }
-    else{
-        errors = getLoginFormErrors(email_input.value, password_input.value)
-    }
-
-    if(errors.length>0){
-        e.preventDefault()
-        error_message.innerText = errors.join(". ")
-    }
-})
-
-function getSignupFormErrors(username, email, password,repeatPassword){
-    let errors = []
-    if(username === '' || username == null){
-        errors.push('Username is required')
-        username_input.parentElement.classList.add('incorrect')
-    }
-    if(email === '' || email == null){
-        errors.push('Email is required')
-        email.parentElement.classList.add('incorrect')
-    }
-    if(password === '' || password == null){
-        errors.push('Password is required')
-        password_input.parentElement.classList.add('incorrect')
-    }
-    return errors;
-}
->>>>>>> 7d90ef21c3aaf9ceeeb3981f3cec1049eb49d3e8
